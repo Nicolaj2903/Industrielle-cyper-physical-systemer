5ab66192df70a4399f4b254ee2e26ef37ebc628a
Nicolaj <70510231+Nicolaj2903@users.noreply.github.com>
2021-09-22 17:49:01 +0200

    Done

--- src/main/java/Calculate_OEE.java
+++ src/main/java/Calculate_OEE.java
@@ -1,35 +1,44 @@
 public class Calculate_OEE {
 
-    // Item & data
-    private double shift_length = 450; // 7,5 hours = 450 minutes
-    private double breaks = 30; // One break of 30 minutes
-    
-    /* Downtime: The amount of time the production is stopped.
-       In this case, downtime is equal to the length of the break */
-    private double downtime = 30; // Minutes 
-    private double idea_cycle_time = 0.025; // 1,5 seconds = 0,025 minutes
-    private double total_count = 5403;
-    private double reject_count = 108;
-
-
-    // Calculate references
-    private double good_count = total_count - reject_count;
-    private double planned_production_time = shift_length - breaks;
-    private double run_time = planned_production_time - downtime;
-
-
-    // The three big ones
-    private double availability = ;
-    private double performance = (ideal_cycle_time * total_count) / run_time;
-    private double quality = good_count / total_count;
-
-
-    // OEE calucations
-    private double oee_1 = (good_count * ideal_cycle_time) / planned_production_time ;
-    private double ooe_2 = availability * performance * quality;
-
     public static void main(String[] args) {
-        System.out.println(ooe_1);
-        System.out.println(ooe_2);
+
+        // Item & data
+        double shift_length = 450; // 7,5 hours = 450 minutes           // Correct
+        double breaks = 30; // One break of 30 minutes
+        double unplanned_downtime = 0;                                   // Correct
+        double ideal_cycle_time = 1.5;
+        double total_count = 5403;
+        double reject_count = 108;
+
+
+        double new_planned_production_time = shift_length - breaks;
+        double operating_time = new_planned_production_time - unplanned_downtime;
+        double new_availability = operating_time / new_planned_production_time;
+        double capacity = (1/ideal_cycle_time) * operating_time;
+        double new_performance = total_count / capacity;
+        double new_quality = (total_count - reject_count) / total_count;
+        double oee = new_availability * new_performance * new_quality;
+
+        System.out.println("\n(Net) OEE: " + oee);
+
+
+        // Calculate references
+        double good_count = total_count - reject_count;
+        double planned_production_time = shift_length - breaks; // Correct
+        double run_time = planned_production_time - unplanned_downtime; // Correct
+
+
+        // The three big ones
+        double availability = run_time / planned_production_time;   // Correct
+        double performance = (ideal_cycle_time * total_count) / run_time;
+        double quality = good_count / total_count;
+
+
+        // OEE calucations
+        double oee_1 = (good_count * ideal_cycle_time) / planned_production_time;
+        double oee_2 = availability * performance * quality;
+
+        System.out.println("\n(Mine) OEE: " + oee_1);
+        System.out.println("(Mine) OEE: " + oee_2);
     }
 }
